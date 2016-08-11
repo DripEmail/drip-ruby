6e2f4138a820643f9166ba85a05bed23855d3d12
John Careaga <johnc219dev@gmail.com>
2016-08-11 12:32:30 -0400

    Implement wrapper for list-campaigns api call

---
+++ lib/drip/client/campaigns.rb
@@ -0,0 +1,21 @@
+module Drip
+  class Client
+    module Campaigns
+      # Public: Fetch campaigns for this account
+      #
+      # options - A Hash of options
+      #           - status - Optional. Filter by one of the following statuses:
+      #                      draft, active, or paused. Defaults to all.
+      #           - page   - Optional. Use this parameter to paginate through
+      #                      your list of campaigns. Each response contains a
+      #                      a `meta` object that includes `total_count` and
+      #                      `total_pages` attributes.
+      #
+      # Returns a Drip::Response.
+      # See https://www.getdrip.com/docs.rest-api#campaigns
+      def campaigns(options = {})
+        get "#{account_id}/campaigns", options
+      end
+    end
+  end
+end
