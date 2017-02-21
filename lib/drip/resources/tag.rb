44d0e6df6782ee4893b294d8852b58e233112d21
Derrick Reimer <derrickreimer@gmail.com>
2017-02-21 08:33:51 -0600

    Remove duplicate initializer

--- lib/drip/resources/tag.rb
+++ lib/drip/resources/tag.rb
@@ -5,10 +5,5 @@ module Drip
     def self.resource_name
       "tag"
     end
-
-    def initialize(raw_data = {})
-      @raw_attributes = raw_data.dup.freeze
-      @attributes = @raw_attributes
-    end
   end
 end
