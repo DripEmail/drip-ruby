eb08bbbd830b8d100641a5044313bab4bd44362c
William Johnston <william.johnston@drip.com>
2018-05-23 13:07:01 -0500

    custom fields test port

--- test/drip/client/custom_fields_test.rb
+++ test/drip/client/custom_fields_test.rb
@@ -2,24 +2,16 @@ require File.dirname(__FILE__) + '/../../test_helper.rb'
 
 class Drip::Client::CustomFieldsTest < Drip::TestCase
   def setup
-    @stubs = Faraday::Adapter::Test::Stubs.new
-
-    @connection = Faraday.new do |builder|
-      builder.adapter :test, @stubs
-    end
-
     @client = Drip::Client.new { |c| c.account_id = "12345" }
-    @client.expects(:connection).at_least_once.returns(@connection)
   end
 
   context "#custom_fields" do
     setup do
       @response_status = 200
-      @response_body = stub
+      @response_body = "stub"
 
-      @stubs.get "12345/custom_field_identifiers" do
-        [@response_status, {}, @response_body]
-      end
+      stub_request(:get, "https://api.getdrip.com/v2/12345/custom_field_identifiers").
+        to_return(status: @response_status, body: @response_body, headers: {})
     end
 
     should "send the right request" do
