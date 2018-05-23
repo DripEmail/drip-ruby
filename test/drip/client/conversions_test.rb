a7e454ec857c4097f2cc67797fd26e412a61ebcb
William Johnston <william.johnston@drip.com>
2018-05-23 13:06:15 -0500

    Port conversions test

--- test/drip/client/conversions_test.rb
+++ test/drip/client/conversions_test.rb
@@ -2,24 +2,16 @@ require File.dirname(__FILE__) + '/../../test_helper.rb'
 
 class Drip::Client::ConversionsTest < Drip::TestCase
   def setup
-    @stubs = Faraday::Adapter::Test::Stubs.new
-
-    @connection = Faraday.new do |builder|
-      builder.adapter :test, @stubs
-    end
-
     @client = Drip::Client.new { |c| c.account_id = "12345" }
-    @client.expects(:connection).at_least_once.returns(@connection)
   end
 
   context "#conversions" do
     setup do
       @response_status = 200
-      @response_body = stub
+      @response_body = "stub"
 
-      @stubs.get "12345/goals" do
-        [@response_status, {}, @response_body]
-      end
+      stub_request(:get, "https://api.getdrip.com/v2/12345/goals").
+        to_return(status: @response_status, body: @response_body, headers: {})
     end
 
     should "send the right request" do
@@ -31,12 +23,11 @@ class Drip::Client::ConversionsTest < Drip::TestCase
   context "#conversion" do
     setup do
       @response_status = 200
-      @response_body = stub
+      @response_body = "stub"
       @id = 9999999
 
-      @stubs.get "12345/goals/#{@id}" do
-        [@response_status, {}, @response_body]
-      end
+      stub_request(:get, "https://api.getdrip.com/v2/12345/goals/#{@id}").
+        to_return(status: @response_status, body: @response_body, headers: {})
     end
 
     should "send the right request" do
