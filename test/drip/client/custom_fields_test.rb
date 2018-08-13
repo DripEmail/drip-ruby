83c6ad4989a108657ebad6958f434b5bf0554cec
William Johnston <william.johnston@drip.com>
2018-08-13 14:43:22 -0500

    Fix collection of strings assuming complex objects. Fixes #30

--- test/drip/client/custom_fields_test.rb
+++ test/drip/client/custom_fields_test.rb
@@ -8,7 +8,15 @@ class Drip::Client::CustomFieldsTest < Drip::TestCase
   context "#custom_fields" do
     setup do
       @response_status = 200
-      @response_body = "{}"
+      @response_body = <<-JSON
+        {
+          "custom_field_identifiers":[
+            "custom_field_1",
+            "custom_field_2",
+            "custom_field_3"
+          ]
+        }
+      JSON
 
       stub_request(:get, "https://api.getdrip.com/v2/12345/custom_field_identifiers").
         to_return(status: @response_status, body: @response_body, headers: {})
