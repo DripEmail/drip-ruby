b65cf70eff8022a50efd15cb4805c3af9b512dee
William Johnston <william.johnston@drip.com>
2019-05-13 15:01:40 -0500

    Add test coverage for deprecated methods.

--- test/drip/client_test.rb
+++ test/drip/client_test.rb
@@ -50,6 +50,13 @@ class Drip::ClientTest < Drip::TestCase
       assert_equal 20, client.http_open_timeout
       assert_equal 25, client.http_timeout
     end
+
+    should "accept options after initialization" do
+      # Deprecated
+      client = Drip::Client.new
+      assert_output(nil, /^\[DEPRECATED\] Setting configuration/) { client.account_id = "12345" }
+      assert_equal "12345", client.account_id
+    end
   end
 
   context "given a basic request" do
@@ -93,6 +100,39 @@ class Drip::ClientTest < Drip::TestCase
       @client.subscriber("blah")
 
       assert_requested :get, "https://api.example.com/v9001/v2/subscribers/blah"
+    end
+  end
+
+  context "#generate_resource" do
+    # Deprecated
+    should "return a resource and note deprecation" do
+      client = Drip::Client.new
+      resource = nil
+      assert_output(nil, /^\[DEPRECATED\] Drip\:\:Client\#generate_resource is deprecated/) { resource = client.generate_resource("hello", {}) }
+      assert_equal({ "hello" => [{}] }, resource)
+    end
+  end
+
+  context "#content_type" do
+    # Deprecated
+    should "return default content type and print warning" do
+      client = Drip::Client.new
+      content_type = nil
+      assert_output(nil, /^\[DEPRECATED\] Drip\:\:Client\#content_type is deprecated/) { content_type = client.content_type }
+      assert_equal "application/vnd.api+json", content_type
+    end
+  end
+
+  context "#get et all" do
+    # Deprecated
+    should "delegate with v2 and print deprecation warning" do
+      stub_request(:get, "https://api.getdrip.com/v2/testpath").
+        to_return(status: 200, body: "{}")
+
+      client = Drip::Client.new
+      response = nil
+      assert_output(nil, /^\[DEPRECATED\] Drip\:\:Client\#get please use the API endpoint specific methods/) { response = client.get("testpath") }
+      assert_equal({}, response.body)
     end
   end
 end
