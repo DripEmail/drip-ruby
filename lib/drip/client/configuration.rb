4fc71077a4e955fbf44d8162f9f0231d15a0f3b1
Jarrett Lusso <jclusso@gmail.com>
2022-03-08 18:38:26 -0500

    Added support for skip_analytics config option.

--- lib/drip/client/configuration.rb
+++ lib/drip/client/configuration.rb
@@ -6,7 +6,10 @@ module Drip
       DEFAULT_URL_PREFIX = "https://api.getdrip.com/"
       private_constant :DEFAULT_URL_PREFIX
 
-      CONFIGURATION_FIELDS = %i[access_token api_key account_id url_prefix http_open_timeout http_timeout].freeze
+      CONFIGURATION_FIELDS = %i[
+        access_token api_key account_id url_prefix http_open_timeout
+        http_timeout skip_analytics
+      ].freeze
 
       attr_accessor(*CONFIGURATION_FIELDS)
 
